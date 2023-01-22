cff3356cbef2159303ce5749826b0459a9fe7b4c
duynghiadev <duynghia22302@gmail.com>
2023-01-22 16:18:03 +0700

    create and validation form in  React (Formik, Yup)

--- react redux reddit/backend/index.js
+++ react redux reddit/backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 app.post("/v1/update", (req, res) => {
   setTimeout(() => {
--- react redux reddit/frontend/src/redux/apiRequests.js
+++ react redux reddit/frontend/src/redux/apiRequests.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const updateUser = async (user, dispatch) => {
   dispatch(updateStart());
   try {
-    const res = await axios.post("/v1/update", user);
+    const res = await axios.post("http://localhost:8000/v1/update", user);
     dispatch(updateSuccess(res.data));
   } catch (err) {
     console.log("error", err);
