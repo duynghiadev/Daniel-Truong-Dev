6e423d014a819be9a51f92fafcf45965470d2b9e
duynghiadev <duynghia22302@gmail.com>
2023-01-25 22:45:20 +0700

    Finish Project Tailwind CSS, React - Xây dựng Landing Page có responsive, dark mode

--- Landing Page TailwindCss/src/App.jsx
+++ Landing Page TailwindCss/src/App.jsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Content from "./components/Content/Content";
 import NavBar from "./components/NavBar/NavBar";
 import Slogan from "./components/Slogan/Slogan";
 
 function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const handleSize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        heigth: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleSize);
+    handleSize();
+    return () => window.removeEventListener("resize", handleSize);
+  }, []);
+
+  useEffect(() => {
+    if (windowSize.width < 500) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [windowSize]);
   return (
-    <h1 className="font-sora overflow-y-auto overflow-x-hidden h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to-purple-700 text-white">
-      <NavBar />
+    <div className="font-sora overflow-y-auto overflow-x-hidden h-screen px-4 py-8 bg-gradient-to-b dark:from-purple-900 dark:to-purple-700 from-white to-pink-500  dark:text-white text-black md:px-20">
+      <NavBar isMobile={isMobile} />
       <Slogan />
       <Content />
-    </h1>
+    </div>
   );
 }
 
